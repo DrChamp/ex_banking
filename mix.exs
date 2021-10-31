27c2eb7cf8cbb57e2dab6e64d60d16382df4bd33
Vikram Singh <vwithn@gmail.com>
2021-10-31 13:40:05 +0530

    lowerd elixir version capablity

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExBanking.MixProject do
     [
       app: :ex_banking,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
